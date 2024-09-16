59d8f0d5aa0fa0b9225762dfc7ebb48c441acb7f
TirumaniNadar <111589721+TirumaniNadar@users.noreply.github.com>
2024-09-16 16:05:47 +0530

    laravel 11

--- resources/views/mail/share-event-mail.blade.php
+++
@@ -1,7 +0,0 @@
-@component('mail::message')
-    ## Details
-
-    - **Code:** {{ $data->code }}
-    - **Message:** {{ $data->message }}
-    - **Link:** {{ $data->register_event_url }}
-@endcomponent
