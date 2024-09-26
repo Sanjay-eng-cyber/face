45c4729664cc321b58beab836d53bcd50d1bd8c2
Raees-Shaikh-Acetrot <raees.shaikh@acetrot.com>
2024-09-26 15:59:17 +0530

    footer add updated

---
+++ resources/views/frontend/layouts/footer.blade.php
@@ -0,0 +1,42 @@
+<section class="mt-5 image">
+    <footer class="footer">
+     <div class="container">
+        <div class="row pt-4 pb-3">
+
+            <div class="col-lg-8 ">
+                <ul class="footer-links">
+                    <li><a href="">Home</a></li>
+                    <li><a href="">About Us</a></li>
+                    <li><a href="">Contact Us</a></li>
+                    <li><a href="">Pricing</a></li>
+                    <li><a href="">Blog</a></li>
+
+                  </ul>
+
+
+
+            </div>
+
+            <div class="col-lg-4 text-right justify-content-end d-flex">
+                <div class="social">
+                    <a href=""> <img src="{{asset('frontend/images/index/fb.png')}}" alt=""></a>
+                    <a href=""> <img src="{{asset('frontend/images/index/wa.png')}}" alt=""></a>
+                    <a href=""> <img src="{{asset('frontend/images/index/twit.png')}}" alt=""></a>
+                    <a href=""> <img src="{{asset('frontend/images/index/insta.png')}}" alt=""></a>
+
+                   </div>
+            </div>
+
+        </div>
+        <div class="bottom-footer text-center">
+            <p class="m-0">Designed & Developed By <a href="https://acetrot.com/" target="_blank">Acetrot</a>.</p>
+
+            <p class="m-0"> <a href="https://acetrot.com/" target="_blank">Privacy Policy</a> | <a href="https://acetrot.com/" target="_blank">Terms & Conditions</a> | <a href="https://acetrot.com/" target="_blank">Refund</a></p>
+
+         </div>
+     </div>
+
+
+
+    </footer>
+</section>
