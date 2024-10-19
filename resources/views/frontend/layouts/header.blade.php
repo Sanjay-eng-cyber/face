dc47200f19847fb6b9831328b21f092de5d40bdf
Raeesh-Rayeen-Acetrot <raeesh.rayeen@acetrot.com>
2024-10-19 16:50:04 +0530

    Montserrat Alternates font family add

--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -1,10 +1,16 @@
-<link rel="stylesheet" href="{{ asset('frontend/css/bootstrap.min.css') }}" />
+{{-- <link rel="stylesheet" href="{{ asset('frontend/css/bootstrap.min.css') }}" /> --}}
 {{-- <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@400;500;600;700;800;900&display=swap" rel="stylesheet"> --}}
+{{-- <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"> --}}
+
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="{{ asset('frontend/css/bootstrap.min.css') }}" />
+<link href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
+
+<link rel="stylesheet" href="{{ asset('frontend/css/bootstrap.min.css') }}" />
 <link rel="stylesheet" href="{{ asset('frontend/css/slick.css') }}" />
 <link rel="stylesheet" href="{{ asset('frontend/css/slick-theme.css') }}" />
 <link rel="stylesheet" href="{{asset('frontend/css/animated-min.css')}}"/>
@@ -13,3 +19,4 @@
 <link rel="stylesheet" href="{{ asset('frontend/css/frontend.css') }}">
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
