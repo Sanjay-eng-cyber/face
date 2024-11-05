e9beea66a436317044c3a50fd25e82ef89125abe
Acetrot-Sanjay <sanjay.ahirwal@acetrot.com>
2024-11-05 13:54:57 +0530

    snackbar add in frontend and success message issue fixes

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -1,19 +1,23 @@
 <section class="">
     <footer class="footer px-3" style="margin-bottom: 58px">
-        <div style="
+        <div
+            style="
             background: linear-gradient(146.76deg, #045959 10.04%, rgba(123, 69, 139, 0) 62.25%);
             padding:1px;
             border-radius:60px;
             ">
             <div style="background: #040404;border-radius:60px;position:relative">
-                <img src="{{asset('frontend/images/index/index-new/fheartone.svg')}}" alt="" srcset="" class="img-fluid" style="position: absolute;z-index: 0;bottom: 0px;">
-                <img src="{{asset('frontend/images/index/index-new/fheattwo.svg')}}" alt="" srcset="" class="img-fluid" style="
+                <img src="{{ asset('frontend/images/index/index-new/fheartone.svg') }}" alt="" srcset=""
+                    class="img-fluid" style="position: absolute;z-index: 0;bottom: 0px;">
+                <img src="{{ asset('frontend/images/index/index-new/fheattwo.svg') }}" alt="" srcset=""
+                    class="img-fluid"
+                    style="
                 position: absolute;
                 z-index: 0;
                 top: 2em;
                 right: 21em;
                 ">
-                <div class="footer-insider" >
+                <div class="footer-insider">
                     <div class="container px-0">
                         <div class="row  pb-3 text-lg-start text-center">
 
@@ -33,28 +37,44 @@
 
                             <div class="col-lg-4 text-right justify-content-lg-end  justify-content-center  d-flex">
                                 <div class="social">
-                                    <a href=""> 
-                                        <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                            <path d="M14 14H16.5L17.5 10H14V8C14 6.97 14 6 16 6H17.5V2.64C17.174 2.597 15.943 2.5 14.643 2.5C11.928 2.5 10 4.157 10 7.2V10H7V14H10V22.5H14V14Z" fill="#FE3B96"/>
-                                        </svg>    
+                                    <a href="">
+                                        <svg width="24" height="25" viewBox="0 0 24 25" fill="none"
+                                            xmlns="http://www.w3.org/2000/svg">
+                                            <path
+                                                d="M14 14H16.5L17.5 10H14V8C14 6.97 14 6 16 6H17.5V2.64C17.174 2.597 15.943 2.5 14.643 2.5C11.928 2.5 10 4.157 10 7.2V10H7V14H10V22.5H14V14Z"
+                                                fill="#FE3B96" />
+                                        </svg>
                                     </a>
                                     <a href="">
-                                        <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                            <path d="M19.0498 5.41005C18.1329 4.48416 17.0408 3.75002 15.8373 3.25042C14.6338 2.75081 13.3429 2.49574 12.0398 2.50005C6.5798 2.50005 2.1298 6.95005 2.1298 12.4101C2.1298 14.1601 2.5898 15.8601 3.4498 17.3601L2.0498 22.5001L7.2998 21.1201C8.7498 21.9101 10.3798 22.3301 12.0398 22.3301C17.4998 22.3301 21.9498 17.8801 21.9498 12.4201C21.9498 9.77005 20.9198 7.28005 19.0498 5.41005ZM12.0398 20.6501C10.5598 20.6501 9.1098 20.2501 7.8398 19.5001L7.5398 19.3201L4.4198 20.1401L5.2498 17.1001L5.0498 16.7901C4.22735 15.4771 3.79073 13.9593 3.7898 12.4101C3.7898 7.87005 7.4898 4.17005 12.0298 4.17005C14.2298 4.17005 16.2998 5.03005 17.8498 6.59005C18.6174 7.35392 19.2257 8.2626 19.6394 9.26338C20.0531 10.2642 20.264 11.3371 20.2598 12.4201C20.2798 16.9601 16.5798 20.6501 12.0398 20.6501ZM16.5598 14.4901C16.3098 14.3701 15.0898 13.7701 14.8698 13.6801C14.6398 13.6001 14.4798 13.5601 14.3098 13.8001C14.1398 14.0501 13.6698 14.6101 13.5298 14.7701C13.3898 14.9401 13.2398 14.9601 12.9898 14.8301C12.7398 14.7101 11.9398 14.4401 10.9998 13.6001C10.2598 12.9401 9.7698 12.1301 9.6198 11.8801C9.4798 11.6301 9.5998 11.5001 9.7298 11.3701C9.8398 11.2601 9.9798 11.0801 10.0998 10.9401C10.2198 10.8001 10.2698 10.6901 10.3498 10.5301C10.4298 10.3601 10.3898 10.2201 10.3298 10.1001C10.2698 9.98005 9.7698 8.76005 9.5698 8.26005C9.3698 7.78005 9.1598 7.84005 9.0098 7.83005H8.5298C8.3598 7.83005 8.0998 7.89005 7.8698 8.14005C7.6498 8.39005 7.0098 8.99005 7.0098 10.2101C7.0098 11.4301 7.89981 12.6101 8.0198 12.7701C8.1398 12.9401 9.7698 15.4401 12.2498 16.5101C12.8398 16.7701 13.2998 16.9201 13.6598 17.0301C14.2498 17.2201 14.7898 17.1901 15.2198 17.1301C15.6998 17.0601 16.6898 16.5301 16.8898 15.9501C17.0998 15.3701 17.0998 14.8801 17.0298 14.7701C16.9598 14.6601 16.8098 14.6101 16.5598 14.4901Z" fill="#FE3B96"/>
-                                        </svg>    
+                                        <svg width="24" height="25" viewBox="0 0 24 25" fill="none"
+                                            xmlns="http://www.w3.org/2000/svg">
+                                            <path
+                                                d="M19.0498 5.41005C18.1329 4.48416 17.0408 3.75002 15.8373 3.25042C14.6338 2.75081 13.3429 2.49574 12.0398 2.50005C6.5798 2.50005 2.1298 6.95005 2.1298 12.4101C2.1298 14.1601 2.5898 15.8601 3.4498 17.3601L2.0498 22.5001L7.2998 21.1201C8.7498 21.9101 10.3798 22.3301 12.0398 22.3301C17.4998 22.3301 21.9498 17.8801 21.9498 12.4201C21.9498 9.77005 20.9198 7.28005 19.0498 5.41005ZM12.0398 20.6501C10.5598 20.6501 9.1098 20.2501 7.8398 19.5001L7.5398 19.3201L4.4198 20.1401L5.2498 17.1001L5.0498 16.7901C4.22735 15.4771 3.79073 13.9593 3.7898 12.4101C3.7898 7.87005 7.4898 4.17005 12.0298 4.17005C14.2298 4.17005 16.2998 5.03005 17.8498 6.59005C18.6174 7.35392 19.2257 8.2626 19.6394 9.26338C20.0531 10.2642 20.264 11.3371 20.2598 12.4201C20.2798 16.9601 16.5798 20.6501 12.0398 20.6501ZM16.5598 14.4901C16.3098 14.3701 15.0898 13.7701 14.8698 13.6801C14.6398 13.6001 14.4798 13.5601 14.3098 13.8001C14.1398 14.0501 13.6698 14.6101 13.5298 14.7701C13.3898 14.9401 13.2398 14.9601 12.9898 14.8301C12.7398 14.7101 11.9398 14.4401 10.9998 13.6001C10.2598 12.9401 9.7698 12.1301 9.6198 11.8801C9.4798 11.6301 9.5998 11.5001 9.7298 11.3701C9.8398 11.2601 9.9798 11.0801 10.0998 10.9401C10.2198 10.8001 10.2698 10.6901 10.3498 10.5301C10.4298 10.3601 10.3898 10.2201 10.3298 10.1001C10.2698 9.98005 9.7698 8.76005 9.5698 8.26005C9.3698 7.78005 9.1598 7.84005 9.0098 7.83005H8.5298C8.3598 7.83005 8.0998 7.89005 7.8698 8.14005C7.6498 8.39005 7.0098 8.99005 7.0098 10.2101C7.0098 11.4301 7.89981 12.6101 8.0198 12.7701C8.1398 12.9401 9.7698 15.4401 12.2498 16.5101C12.8398 16.7701 13.2998 16.9201 13.6598 17.0301C14.2498 17.2201 14.7898 17.1901 15.2198 17.1301C15.6998 17.0601 16.6898 16.5301 16.8898 15.9501C17.0998 15.3701 17.0998 14.8801 17.0298 14.7701C16.9598 14.6601 16.8098 14.6101 16.5598 14.4901Z"
+                                                fill="#FE3B96" />
+                                        </svg>
                                     </a>
                                     <a href="">
-                                        <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                            <path d="M13.795 11.033L20.68 2.5H17.607L12.352 9.017L7.69 2.5H1L8.806 13.41L1.47 22.5H4.544L10.249 15.43L15.31 22.5H22L13.795 11.033ZM11.415 13.983L9.97 11.964L4.36 4.127H6.67L11.198 10.444L12.641 12.464L18.659 20.873H16.349L11.415 13.983Z" fill="#FE3B96"/>
-                                            </svg>
-                                            
+                                        <svg width="24" height="25" viewBox="0 0 24 25" fill="none"
+                                            xmlns="http://www.w3.org/2000/svg">
+                                            <path
+                                                d="M13.795 11.033L20.68 2.5H17.607L12.352 9.017L7.69 2.5H1L8.806 13.41L1.47 22.5H4.544L10.249 15.43L15.31 22.5H22L13.795 11.033ZM11.415 13.983L9.97 11.964L4.36 4.127H6.67L11.198 10.444L12.641 12.464L18.659 20.873H16.349L11.415 13.983Z"
+                                                fill="#FE3B96" />
+                                        </svg>
+
                                     </a>
                                     <a href="" class="mr-0">
-                                        <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                            <path d="M2.5 12.5C2.5 8.022 2.5 5.782 3.891 4.391C5.282 3 7.521 3 12 3C16.478 3 18.718 3 20.109 4.391C21.5 5.782 21.5 8.021 21.5 12.5C21.5 16.978 21.5 19.218 20.109 20.609C18.718 22 16.479 22 12 22C7.522 22 5.282 22 3.891 20.609C2.5 19.218 2.5 16.979 2.5 12.5Z" stroke="#FE3B96" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                                            <path d="M17.508 7H17.498M16.5 12.5C16.5 13.6935 16.0259 14.8381 15.182 15.682C14.3381 16.5259 13.1935 17 12 17C10.8065 17 9.66193 16.5259 8.81802 15.682C7.97411 14.8381 7.5 13.6935 7.5 12.5C7.5 11.3065 7.97411 10.1619 8.81802 9.31802C9.66193 8.47411 10.8065 8 12 8C13.1935 8 14.3381 8.47411 15.182 9.31802C16.0259 10.1619 16.5 11.3065 16.5 12.5Z" stroke="#FE3B96" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                                            </svg>
-                                            
+                                        <svg width="24" height="25" viewBox="0 0 24 25" fill="none"
+                                            xmlns="http://www.w3.org/2000/svg">
+                                            <path
+                                                d="M2.5 12.5C2.5 8.022 2.5 5.782 3.891 4.391C5.282 3 7.521 3 12 3C16.478 3 18.718 3 20.109 4.391C21.5 5.782 21.5 8.021 21.5 12.5C21.5 16.978 21.5 19.218 20.109 20.609C18.718 22 16.479 22 12 22C7.522 22 5.282 22 3.891 20.609C2.5 19.218 2.5 16.979 2.5 12.5Z"
+                                                stroke="#FE3B96" stroke-width="1.5" stroke-linecap="round"
+                                                stroke-linejoin="round" />
+                                            <path
+                                                d="M17.508 7H17.498M16.5 12.5C16.5 13.6935 16.0259 14.8381 15.182 15.682C14.3381 16.5259 13.1935 17 12 17C10.8065 17 9.66193 16.5259 8.81802 15.682C7.97411 14.8381 7.5 13.6935 7.5 12.5C7.5 11.3065 7.97411 10.1619 8.81802 9.31802C9.66193 8.47411 10.8065 8 12 8C13.1935 8 14.3381 8.47411 15.182 9.31802C16.0259 10.1619 16.5 11.3065 16.5 12.5Z"
+                                                stroke="#FE3B96" stroke-width="1.5" stroke-linecap="round"
+                                                stroke-linejoin="round" />
+                                        </svg>
+
                                     </a>
 
                                 </div>
@@ -62,11 +82,55 @@
 
                         </div>
                         <div class="bottom-footer text-center d-md-flex d-block pt-md-4 pb-md-0 pt-3 pb-0">
-                            <p class="m-0 mt-md-0 mt-2 py-md-2">Designed & Developed By <a href="https://acetrot.com/" target="_blank">Acetrot</a>.</p>
+                            <p class="m-0 mt-md-0 mt-2 py-md-2">Designed & Developed By <a href="https://acetrot.com/"
+                                    target="_blank">Acetrot</a>.</p>
 
-                            <p class="m-0 mt-md-0 mt-2 py-md-2"> <a href="https://acetrot.com/" target="_blank">Privacy Policy</a> | <a href="https://acetrot.com/" target="_blank">Terms & Conditions</a> | <a href="https://acetrot.com/" target="_blank">Refund</a></p>
+                            <p class="m-0 mt-md-0 mt-2 py-md-2"> <a href="https://acetrot.com/" target="_blank">Privacy
+                                    Policy</a> | <a href="https://acetrot.com/" target="_blank">Terms & Conditions</a> |
+                                <a href="https://acetrot.com/" target="_blank">Refund</a>
+                            </p>
 
                         </div>
+                        <script src="{{ asset('backend/plugins/notification/snackbar/snackbar.min.js') }}"></script>
+                        <script>
+                            @if (Session::get('alert-type') == 'success')
+                                @if (Session::has('message'))
+                                    Snackbar.show({
+                                        text: "{{ Session::get('message') }}",
+                                        pos: 'top-right',
+                                        actionTextColor: '#fff',
+                                        backgroundColor: '#1abc9c'
+                                    });
+                                @endif
+                            @elseif (Session::get('alert-type') == 'info')
+                                @if (Session::has('message'))
+                                    Snackbar.show({
+                                        text: "{{ Session::get('message') }}",
+                                        pos: 'top-right',
+                                        actionTextColor: '#fff',
+                                        backgroundColor: '#2196f3'
+                                    });
+                                @endif
+                            @elseif (Session::get('alert-type') == 'error')
+                                @if (Session::has('message'))
+                                    Snackbar.show({
+                                        text: "{{ Session::get('message') }}",
+                                        pos: 'top-right',
+                                        actionTextColor: '#fff',
+                                        backgroundColor: '#e7515a'
+                                    });
+                                @endif
+                            @else
+                                @if (Session::has('message'))
+                                    Snackbar.show({
+                                        text: "{{ Session::get('message') }}",
+                                        pos: 'top-right',
+                                        actionTextColor: '#fff',
+                                        backgroundColor: '#3b3f5c'
+                                    });
+                                @endif
+                            @endif
+                        </script>
                     </div>
                 </div>
             </div>
