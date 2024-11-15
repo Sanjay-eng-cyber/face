594a25bde2149e0c9a4a4ccb8d914554201b56eb
Raeesh-Rayeen-Acetrot <raeesh.rayeen@acetrot.com>
2024-11-15 16:19:35 +0530

    basic  details two responsive updated

--- resources/views/frontend/basic-details-two.blade.php
+++ resources/views/frontend/basic-details-two.blade.php
@@ -26,6 +26,9 @@
     .blurhero-img {
             display: none;
         }
+        .smalllarrow-img2{
+            display: none;
+        }
 </style>
 @endsection
 @section('content')
@@ -42,65 +45,65 @@
             <img src="{{asset('frontend/images/gallery/bigarrow.svg')}}" alt="" srcset="" class="img-fluid bigarrow-img" style="z-index: -99">
            
             <div class="container overflow-hide">
-                <div class="row  pt-35px">
-                  <div class="col-6 col-xl-6 col-xxl-5">
+                <div class="row  pt-35px pb-30">
+                  <div class="col-12 col-lg-10 col-xl-9 col-xxl-8">
                     
-                            
-                          <div class="basic-event-one-main" >
-                            <div class="basic-event-one-main-insider" >
+                       
+                          <div class="basic-event-one-main-bdt" >
+                            <div class="basic-event-one-main-insider-bdt" >
                                 <div>
-                                    <img src="{{ asset('frontend/images/basic-event-one/ex-one.png') }}" alt="" class="img-fluid ex-one-img" >
+                                    <img src="{{ asset('frontend/images/basic-event-one/ex-one.png') }}" alt="" class="img-fluid ex-one-img w-100" >
                                 </div>
                                 <div>
-                                    <div class="eventanddatespit ">
-                                        <div class="h5 fw-600 mb-0 text-white">Business event</div>
-                                        <div class="text-white fw-300 fs-14" >10/09/2024 to 14/09/2024</div>
+                                    <div class="eventanddatespit-bdt ">
+                                        <div class="h5 fw-600 mb-0 text-white bdt-eventname">Business event</div>
+                                        <div class="text-white fw-300 fs-14 bdt-date" >10/09/2024 to 14/09/2024</div>
                                     </div>
-                                    <div class="text-white pt-3 fs-14" >
+                                    <div class="text-white pt-2 pt-md-3 fs-14 bdt-date-longpara" >
                                         Picscan is the world's only end-to-end AI-powered image post-production solution.
                                     </div>
                                 </div>
                             </div>
                           </div>
-                        
+                   
                       
 
                   </div>
                 </div>
 
-                <div class="row gx-5">
-                    <div class="col-5 col-xl-6 col-xxl-5">
+                <div class="row gx-3 gx-lg-4 gx-xxl-5">
+                    <div class="col-12 col-lg-5 col-xl-5 col-xxl-5 order-br">
                         <form action="{{ route('frontend.login.submit') }}" method="post" class="login-form pt-4">
                             @csrf
-                            <div class="dblwhitecolor h4 mb-0 fw-600 pb-2">Details</div>
+                            <div class="dblwhitecolor h4 mb-0 fw-600 pb-2 form-details">Details</div>
                             
-                            <div class="pb-3">
-                                <label for="name" class="fw-600 frtwhitcolor pb-2">Name</label>
-                                <input type="text" name="name" minlength="8" maxlength="30" required
-                                    class="form-control sin-input">
-                            </div>
-                            {{-- @if ($errors->has('name'))
-                                <div class="text-danger text-left mx-3" role="alert">{{ $errors->first('name') }}
+                                <div class="pb-2 pb-sm-3">
+                                    <label for="name" class="fw-600 frtwhitcolor pb-2 name-form">Name</label>
+                                    <input type="text" name="name" minlength="8" maxlength="30" required
+                                        class="form-control sin-input">
                                 </div>
-                            @endif --}}
-
-                            <div class="pb-3">
-                                <label for="email" class="fw-600 frtwhitcolor pb-2">Email ID*</label>
-                                <input type="email" name="email" minlength="8" maxlength="30" required
-                                    class="form-control sin-input">
-                            </div>
-                            {{-- @if ($errors->has('email'))
-                                <div class="text-danger text-left mx-3" role="alert">{{ $errors->first('email') }}
-                                </div>
-                            @endif --}}
-                            <div class="pb-3">
-                                <label for="num" class="fw-600 frtwhitcolor pb-2">Number</label>
-                                <input type="text" id="num" name="num" minlength="6" maxlength="6" required class="form-control sin-input">
-                                {{--                                 
-                                @if ($errors->has('num'))
-                                    <div class="text-danger text-left mt-2" role="alert">{{ $errors->first('num') }}</div>
+                                {{-- @if ($errors->has('name'))
+                                    <div class="text-danger text-left mx-3" role="alert">{{ $errors->first('name') }}
+                                    </div>
                                 @endif --}}
-                            </div>
+
+                                <div class="pb-2 pb-sm-3">
+                                    <label for="email" class="fw-600 frtwhitcolor pb-2 email-form">Email ID*</label>
+                                    <input type="email" name="email" minlength="8" maxlength="30" required
+                                        class="form-control sin-input">
+                                </div>
+                                {{-- @if ($errors->has('email'))
+                                    <div class="text-danger text-left mx-3" role="alert">{{ $errors->first('email') }}
+                                    </div>
+                                @endif --}}
+                                <div class="pb-3 pb-sm-3">
+                                    <label for="num" class="fw-600 frtwhitcolor pb-2 num-form">Number</label>
+                                    <input type="text" id="num" name="num" minlength="6" maxlength="6" required class="form-control sin-input">
+                                    {{--                                 
+                                    @if ($errors->has('num'))
+                                        <div class="text-danger text-left mt-2" role="alert">{{ $errors->first('num') }}</div>
+                                    @endif --}}
+                                </div>
                             
                           
                             <div>
@@ -119,7 +122,7 @@
                         </form>
 
                     </div>
-                    <div class="col-7 col-xl-6 col-xxl-7">
+                    <div class="col-12 col-lg-7 col-xl-7 col-xxl-7 order-bl">
                         <div class="two-container-grp">
                             <div class="basic-face-box">
                                 <div class="scan-face-box-insider-twopage" >
@@ -143,7 +146,7 @@
                                 </div>
                             </div>
                             <div class="upload-section ">
-                                <div class="pb-4 browsertext"  >
+                                <div class="pb-4 browsertext brsr-14pxtx">
                                     Browse File
                                 </div>
                                 <div class="d-flex justify-content-center pb-3">
