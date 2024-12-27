59cf8bf147f96b965f2d94b6c7b58784048eb614
Acetrot-Sanjay <sanjay.ahirwal@acetrot.com>
2024-12-27 17:57:42 +0530

    make route and nav for admin only

--- resources/views/backend/statistics/index.blade.php
+++ resources/views/backend/statistics/index.blade.php
@@ -61,30 +61,30 @@
             <div class="statbox widget box box-shadow">
                 <div class="widget-content widget-content-area">
                     <div class="row layout-top-spacing">
-
-                        <div class="col-xl-4 col-md-6 col-sm-12 col-12 layout-spacing mt-sm-0 mt-3">
-                            <div class="widget widget-one_hybrid widget-referral h-100 px-2 py-md-3 py-4">
-                                <div class="widget-heading p-0 m-0">
-                                    <h4 class="text-center">Total Events</h4>
-                                    <div class="w-title justify-content-center align-items-center">
-                                        <div class="w-icon">
-                                            <i class="far fa-chart-bar big-font-icon"> </i>
+                        @cmsUserRole('admin')
+                            <div class="col-xl-4 col-md-6 col-sm-12 col-12 layout-spacing mt-sm-0 mt-3">
+                                <div class="widget widget-one_hybrid widget-referral h-100 px-2 py-md-3 py-4">
+                                    <div class="widget-heading p-0 m-0">
+                                        <h4 class="text-center">Total Events</h4>
+                                        <div class="w-title justify-content-center align-items-center">
+                                            <div class="w-icon">
+                                                <i class="far fa-chart-bar big-font-icon"> </i>
+                                            </div>
+                                            <div class="">
+                                                <p class="w-value">{{ $totalEvents }}</p>
+                                            </div>
                                         </div>
-                                        <div class="">
-                                            <p class="w-value">{{ $totalEvents }}</p>
+                                        <div class="text-center">
+                                            <a href="{{ route('backend.event.index') }}"
+                                                class="btn btn-outline-primary mx-auto">
+                                                <small><i class="far fa-edit"> </i></small> Manage
+                                            </a>
                                         </div>
-                                    </div>
-                                    <div class="text-center">
-                                        <a href="{{ route('backend.event.index') }}"
-                                            class="btn btn-outline-primary mx-auto">
-                                            <small><i class="far fa-edit"> </i></small> Manage
-                                        </a>
                                     </div>
                                 </div>
                             </div>
-                        </div>
 
-                        <div class="col-xl-4 col-md-6 col-sm-12 col-12 layout-spacing mt-sm-0 mt-3">
+                            {{-- <div class="col-xl-4 col-md-6 col-sm-12 col-12 layout-spacing mt-sm-0 mt-3">
                             <div class="widget widget-one_hybrid widget-referral h-100 px-2 py-md-3 py-4">
                                 <div class="widget-heading p-0 m-0">
                                     <h4 class="text-center">Total Users</h4>
@@ -103,29 +103,31 @@
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        </div> --}}
 
-                        <div class="col-xl-4 col-md-6 col-sm-12 col-12 layout-spacing mt-sm-0 mt-3">
-                            <div class="widget widget-one_hybrid widget-referral h-100 px-2 py-md-3 py-4">
-                                <div class="widget-heading p-0 m-0">
-                                    <h4 class="text-center">Total Categories</h4>
-                                    <div class="w-title justify-content-center align-items-center">
-                                        <div class="w-icon">
-                                            <i class="far fa-chart-bar big-font-icon"> </i>
+                            <div class="col-xl-4 col-md-6 col-sm-12 col-12 layout-spacing mt-sm-0 mt-3">
+                                <div class="widget widget-one_hybrid widget-referral h-100 px-2 py-md-3 py-4">
+                                    <div class="widget-heading p-0 m-0">
+                                        <h4 class="text-center">Total Categories</h4>
+                                        <div class="w-title justify-content-center align-items-center">
+                                            <div class="w-icon">
+                                                <i class="far fa-chart-bar big-font-icon"> </i>
+                                            </div>
+                                            <div class="">
+                                                <p class="w-value">{{ $totalCategories }}</p>
+                                            </div>
                                         </div>
-                                        <div class="">
-                                            <p class="w-value">{{ $totalCategories }}</p>
+                                        <div class="text-center">
+                                            <a href="{{ route('backend.category.index') }}"
+                                                class="btn btn-outline-primary mx-auto">
+                                                <small><i class="far fa-edit"> </i></small> Manage
+                                            </a>
                                         </div>
-                                    </div>
-                                    <div class="text-center">
-                                        <a href="{{ route('backend.category.index') }}"
-                                            class="btn btn-outline-primary mx-auto">
-                                            <small><i class="far fa-edit"> </i></small> Manage
-                                        </a>
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        @endcmsUserRole
+
                         @cmsUserRole('super-admin')
                             <div class="col-xl-4 col-md-6 col-sm-12 col-12 layout-spacing mt-sm-0 mt-3">
                                 <div class="widget widget-one_hybrid widget-referral h-100 px-2 py-md-3 py-4">
