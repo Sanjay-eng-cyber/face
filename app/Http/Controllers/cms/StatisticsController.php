4a30fc8cc8f5cac6e811855aa3459e5f6b41c194
Acetrot-Sanjay <sanjay.ahirwal@acetrot.com>
2025-01-04 16:57:28 +0530

    show all event count for super admin and show only that event of user.

--- app/Http/Controllers/cms/StatisticsController.php
+++ app/Http/Controllers/cms/StatisticsController.php
@@ -4,21 +4,25 @@ namespace App\Http\Controllers\cms;
 
 use App\Models\User;
 use App\Models\Event;
+use App\Models\CmsUser;
 use App\Models\Product;
 use App\Models\Category;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Models\CmsUser;
+use Illuminate\Support\Facades\Auth;
 
 class StatisticsController extends Controller
 {
     public function index()
     {
-        // dd(request()->route()->getName());
-        $totalUsers = User::count();
-        $totalEvents = Event::count();
+        $user = Auth::guard('admin')->user('id');
+        if ($user->role == 'admin') {
+            $totalEvents = Event::where('cms_user_id', $user->id)->count();
+        } else {
+            $totalEvents = Event::count();
+        }
         $totalCategories = Category::count();
         $totalCmsUsers = CmsUser::count();
-        return view('backend.statistics.index', compact('totalUsers', 'totalEvents', 'totalCategories', 'totalCmsUsers'));
+        return view('backend.statistics.index', compact('totalEvents', 'totalCategories', 'totalCmsUsers'));
     }
 }
