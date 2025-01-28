40a7726d8aa3886941788de42d9f60d837fe0604
Raeesh-Rayeen-Acetrot <raeesh.rayeen@acetrot.com>
2025-01-28 12:21:50 +0530

    index page update

--- resources/views/backend/login.blade.php
+++ resources/views/backend/login.blade.php
@@ -39,7 +39,7 @@
             background-color:#FE3B96;
             border-radius:12px;
             color: white;
-            border: 0 solid transparent;
+            border: 1px solid transparent;
             transition: none;
             padding: 10.5px 35px;
             width: 100%;
@@ -50,20 +50,19 @@
 
         }
 
-        .bg-pink-color:hover {
+        /* .bg-pink-color:hover {
             background-color: transparent;
             color: #64002F;
-            border: 2px solid #64002F;
-        }
+            border: 1px solid #64002F;
+        } */
 
-        input[data-custom-attribute="channels-login"] {
-            background:#1A1A1A;
-            
+        /* input[data-custom-attribute="channels-login"] {
+            background:#1A1A1A;     
             box-shadow: 8.03325px 11.6847px 15.3362px 1.46059px rgba(0, 0, 0, 0.13);
             border-radius: 10.9544px;
             
 
-        }
+        } */
         .form-control:focus{
             background:#1A1A1A;
 
@@ -88,33 +87,71 @@
         }
         .auth-form-inner{
             background: linear-gradient(165.93deg, rgba(48, 46, 46, 0.14) -2%, rgba(84, 7, 43, 0.14) 105.79%);
-            padding: 30px 59px 59px 60px;
+            padding: 36px 59px 59px 60px;
             border-radius: 25px;
+            display: flex;
+        flex-direction: column;
+        align-items: center;
 
         }
         .auth-form-inner-one{
             background: black;
             border-radius: 25px;
         }
+
+
+
+        .auth-form-input input::placeholder {
+    color: #8C9187; 
+    background:#1A1A1A;     
+
+}
+
+.cmn-input {
+    color: #ffffff; 
+    background: #1A1A1A !important;
+}
+
+.cmn-input:focus {
+    outline: none; 
+    background-color: transparent; 
+    color: white;
+    border: none;
+}
+
+.z-index-1{
+    z-index: -1;
+}
+.login-form{
+    max-width: 441px;
+}
+
+
+@media screen and (max-width:576px) {
+    .auth-form-inner{
+        padding: 25px 15px 25px 15px;
+    }
+}
+
     </style>
 </head>
 
 <body class="auth-page-bg body-bg">
     <img src="{{ asset('backend/images/login/ele-login.svg') }}" alt="" srcset=""
-        class="img-fluid position-absolute start-0 top-0">
+        class="img-fluid position-absolute start-0 top-0 z-index-1">
     <div class="container h-100 d-flex  align-items-center">
 
         <div class="auth-form mx-auto" style="width:694px;">
             <div class="auth-form-inner-one">
                 <div class="auth-form-inner">
-                <a href="{{ url('/') }}"> <img class="d-block mx-auto " style="max-width:47%;border-radius:10px"
+                <a href="{{ url('/') }}"> <img class="d-block mx-auto " style=""
                         src="{{ asset('backend/images/login/dmy-logo.png') }}" alt=""></a>
                 <h1 class="text-center h2 mt-5 auth-text-primary cubold text-cl text-white">Login</h1>
-                <form method="POST" action="{{ route('cms.login.submit') }}">
+                <form method="POST" action="{{ route('cms.login.submit') }}" class="login-form">
                     @csrf
                     <div class="auth-form-input mt-5">
                         <img class="icon" src="backend/images/icon-user.svg" draggable="false">
-                        <input class="form-control input-style" placeholder="Username " id="email" name="email"
+                        <input class="form-control input-style cmn-input" placeholder="Username " id="email" name="email"
                             type="email" minlength="8" maxlength="30" required data-custom-attribute="channels-login">
                     </div>
                     @if ($errors->has('email'))
@@ -122,7 +159,7 @@
                     @endif
                     <div class="auth-form-input mt-4">
                         <img class="icon eye-show-pass" src="backend/images/icon-eye.svg" draggable="false">
-                        <input class="form-control password" type="password" placeholder="Password" id="password"
+                        <input class="form-control password cmn-input" type="password" placeholder="Password" id="password"
                             name="password" minlength="8" maxlength="16" required data-custom-attribute="channels-login">
                     </div>
                     @if ($errors->has('password'))
