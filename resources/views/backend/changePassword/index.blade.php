4500ac09a5e64b091c2da492350fbbe6bdfd1936
Raeesh-Rayeen-Acetrot <raeesh.rayeen@acetrot.com>
2025-01-29 17:54:59 +0530

    content margin fix

--- resources/views/backend/changePassword/index.blade.php
+++ resources/views/backend/changePassword/index.blade.php
@@ -22,7 +22,7 @@
             <div class="row">
                 <div class="col-12 col-lg-12 col-md-10 widget box box-shadow ">
                     <div class="statbox widget box box-shadow">
-                        <h4>Update Profile</h4>
+                        <h4 class="text-clr">Update Profile</h4>
                         <form class="mt-3" method="POST" action="{{ route('cms.password.submit', $user->id) }}"
                             enctype="multipart/form-data">
                             @csrf
