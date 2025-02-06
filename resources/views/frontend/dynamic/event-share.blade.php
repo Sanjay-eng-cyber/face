88f0b752ba02280b6b268aa228af430282c0fd7c
Raeesh-Rayeen-Acetrot <115560229+Raeesh-Rayeen-Acetrot@users.noreply.github.com>
2025-02-06 19:13:40 +0530

    step two img fix

--- resources/views/frontend/dynamic/event-share.blade.php
+++ resources/views/frontend/dynamic/event-share.blade.php
@@ -211,7 +211,7 @@
                                     <div class="basic-event-one-main-insider-bdt">
                                         <div>
                                             <img src="{{ asset('storage/images/events/' . $event->cover_image) }}"
-                                                alt="" class="img-fluid ex-one-img w-100">
+                                                alt="" class="img-fluid ex-one-img-new-fi rounded-2">
                                         </div>
                                         <div>
                                             <div class="eventanddatespit-bdt ">
