7059aad15fd954b2dbf284257bc921c7a551e1ef
Raeesh-Rayeen-Acetrot <115560229+Raeesh-Rayeen-Acetrot@users.noreply.github.com>
2025-02-20 18:26:01 +0530

    footer always at bottom

--- resources/views/backend/event/index.blade.php
+++ resources/views/backend/event/index.blade.php
@@ -1,7 +1,8 @@
 @extends('backend.layouts.app')
 @section('title', 'Events')
 @section('content')
-    <div class="row layout-top-spacing m-0 pa-padding-remove">
+    <div class="layout-px-spacing row layout-top-spacing m-0">
+
         <div id="tableDropdown" class="col-lg-12 col-12 layout-spacing">
 
             <div class="statbox widget box box-shadow mt-3 mb-1">
