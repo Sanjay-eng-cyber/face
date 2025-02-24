8cc8e3c7914421ac78358b709e91a524bcca3648
Raeesh-Rayeen-Acetrot <115560229+Raeesh-Rayeen-Acetrot@users.noreply.github.com>
2025-02-24 11:30:13 +0530

    box card fix

--- resources/views/backend/statistics/index.blade.php
+++ resources/views/backend/statistics/index.blade.php
@@ -75,10 +75,10 @@
                         <div class="widget-content widget-content-area p-0">
                             <div class="row layout-top-spacing p-0">
                                     <div class="col-xl-4 col-md-6 col-sm-12 col-12 mb-4">
-                                        <div class="widget widget-one_hybrid widget-referral h-100 px-2 py-md-3 py-4 d-flex align-items-center justify-content-center">
+                                        <div class="widget widget-one_hybrid widget-referral   d-flex flex-column justify-content-center">
                                             <div class="widget-heading mb-0 p-0">
 
-                                                <div class="w-title justify-content-center align-items-center">
+                                                <div class="w-title justify-content-between align-items-center">
                                                     <h4 class="text-center text-white-2">Total Events</h4>
                                                     <div class="d-flex">
                                                         <div class="w-icon">
@@ -90,7 +90,7 @@
                                                     </div>
                                                 </div>
 
-                                                <div class="text-center">
+                                                <div>
                                                     <a href="{{ route('backend.event.index') }}"
                                                         class="btn btn-outline-primary mx-auto">
                                                         <small><i class="far fa-edit"> </i></small> Manage
@@ -99,20 +99,25 @@
                                             </div>
                                         </div>
                                     </div>
+
 
                                     <div class="col-xl-4 col-md-6 col-sm-12 col-12 mb-4">
-                                        <div class="widget widget-one_hybrid widget-referral h-100 px-2 py-md-3 py-4">
-                                            <div class="widget-heading py-4 m-0">
-                                                <h4 class="text-center text-white-2">Total Users</h4>
-                                                <div class="w-title justify-content-center align-items-center">
-                                                    <div class="w-icon">
-                                                        <i class="far fa-chart-bar big-font-icon"> </i>
-                                                    </div>
-                                                    <div class="">
-                                                        <p class="w-value text-white-2">{{ $totalEvents }}</p>
+                                        <div class="widget widget-one_hybrid widget-referral   d-flex flex-column justify-content-center">
+                                            <div class="widget-heading mb-0 p-0">
+
+                                                <div class="w-title justify-content-between  align-items-center">
+                                                    <h4 class="text-center text-white-2">Total Users</h4>
+                                                    <div class="d-flex">
+                                                        <div class="w-icon">
+                                                            <i class="far fa-chart-bar big-font-icon"> </i>
+                                                        </div>
+                                                        <div class="">
+                                                            <p class="w-value text-white-2">{{ $totalEvents }}</p>
+                                                        </div>
                                                     </div>
                                                 </div>
-                                                <div class="text-center">
+
+                                                <div>
                                                     <a href="{{ route('backend.event.index') }}"
                                                         class="btn btn-outline-primary mx-auto">
                                                         <small><i class="far fa-edit"> </i></small> Manage
@@ -121,20 +126,26 @@
                                             </div>
                                         </div>
                                     </div>
+                                    <div class="col-xl-4 col-md-6 col-sm-12 col-12 mb-4">
+                                        <div class="widget widget-one_hybrid widget-referral  d-flex flex-column justify-content-center">
+                                            <div class="widget-heading mb-0 p-0">
 
-                                    <div class="col-xl-4 col-md-6 col-sm-12 col-12  mb-4">
-                                        <div class="widget widget-one_hybrid widget-referral h-100 px-2 py-md-3 py-4">
-                                            <div class="widget-heading py-4 m-0">
-                                                <h4 class="text-center text-white-2">Total Categories</h4>
-                                                <div class="w-title justify-content-center align-items-center">
-                                                    <div class="w-icon">
-                                                        <i class="far fa-chart-bar big-font-icon"> </i>
-                                                    </div>
-                                                    <div class="">
-                                                        <p class="w-value text-white-2">{{ $totalCategories }}</p>
+                                                <div class="w-title justify-content-between  align-items-center">
+                                                    <h4 class="text-center text-white-2">Total Categories</h4>
+                                                    <div class="d-flex">
+                                                        <div class="w-icon">
+                                                            <i class="far fa-chart-bar big-font-icon"> </i>
+                                                        </div>
+                                                        <div class="">
+                                                            <p class="w-value text-white-2">
+                                                                {{ $totalCategories }}
+
+                                                            </p>
+                                                        </div>
                                                     </div>
                                                 </div>
-                                                <div class="text-center">
+
+                                                <div>
                                                     <a href="{{ route('backend.category.index') }}"
                                                         class="btn btn-outline-primary mx-auto">
                                                         <small><i class="far fa-edit"> </i></small> Manage
@@ -144,28 +155,44 @@
                                         </div>
                                     </div>
 
+                                    
+
+                                   
+
+                                    
+
                                 @cmsUserRole('super-admin')
-                                    <div class="col-xl-4 col-md-6 col-sm-12 col-12  mb-4">
-                                        <div class="widget widget-one_hybrid widget-referral h-100 px-2 py-md-3 py-4">
-                                            <div class="widget-heading py-4 m-0">
+
+                                <div class="col-xl-4 col-md-6 col-sm-12 col-12 mb-4">
+                                    <div class="widget widget-one_hybrid widget-referral d-flex flex-column justify-content-center">
+                                        <div class="widget-heading mb-0 p-0">
+
+                                            <div class="w-title justify-content-between align-items-center">
                                                 <h4 class="text-center text-white-2">Total Cms Users</h4>
-                                                <div class="w-title justify-content-center align-items-center">
+                                                <div class="d-flex">
                                                     <div class="w-icon">
                                                         <i class="far fa-chart-bar big-font-icon"> </i>
                                                     </div>
                                                     <div class="">
-                                                        <p class="w-value text-white-2">{{ $totalCmsUsers }}</p>
+                                                        <p class="w-value text-white-2">
+                                                            {{ $totalCmsUsers }}
+                                                        </p>
                                                     </div>
                                                 </div>
-                                                <div class="text-center">
-                                                    <a href="{{ route('backend.cms-user.index') }}"
-                                                        class="btn btn-outline-primary mx-auto">
-                                                        <small><i class="far fa-edit"> </i></small> Manage
-                                                    </a>
-                                                </div>
+                                            </div>
+
+                                            <div>
+                                                <a href="{{ route('backend.cms-user.index') }}"
+                                                class="btn btn-outline-primary mx-auto">
+                                                    <small><i class="far fa-edit"> </i></small> Manage
+                                                </a>
                                             </div>
                                         </div>
                                     </div>
+                                </div>
+
+
+
                                 @endcmsUserRole
                             </div>
                         </div>
