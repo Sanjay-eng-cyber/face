5b190e786f3a5d8317202c4db78e80baf66b8dae
Raeesh-Rayeen-Acetrot <115560229+Raeesh-Rayeen-Acetrot@users.noreply.github.com>
2025-02-28 13:09:49 +0530

    add arrow in category

--- resources/views/backend/category/create.blade.php
+++ resources/views/backend/category/create.blade.php
@@ -3,6 +3,13 @@
 @section('content')
     <div class="layout-px-spacing row layout-top-spacing m-0">
         <div id="tableDropdown" class="col-lg-12 col-12 layout-spacing">
+            <a href="javascript:void(0);" class="sidebarCollapse arrow-main-btn d-none d-lg-block" data-placement="bottom" style="width:33px;" >
+                <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <rect width="32" height="32" rx="15" fill="#D9D9D9"/>
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M11.8431 16.7111L17.5001 22.3681L18.9141 20.9541L13.9641 16.0041L18.9141 11.0541L17.5001 9.64014L11.8431 15.2971C11.6556 15.4847 11.5503 15.739 11.5503 16.0041C11.5503 16.2693 11.6556 16.5236 11.8431 16.7111Z" fill="black"/>
+                </svg>    
+            </a>
+            
             <div class="statbox widget box box-shadow my-1">
                 <div class="widget-header">
                     <div class="row justify-content-between align-items-center ">
