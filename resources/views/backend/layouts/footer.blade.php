8500ad070b52df4f8b9a6d5e33f3085b85b586d8
Acetrot-Sanjay <sanjay.ahirwal@acetrot.com>
2025-03-11 17:44:34 +0530

    footer make acc to mobile view also

--- resources/views/backend/layouts/footer.blade.php
+++ resources/views/backend/layouts/footer.blade.php
@@ -1,15 +1,35 @@
 <div class="footer-wrapper justify-content-center align-items-end">
     <div class="footer-section f-section-1 text-center pt-sm-1 pt-3">
-        <p class="text-white">
+        <p class="text-white footer-text">
             &copy;
             {{ date('Y') }}
-            {{ config('app.name') }}. All Rights Reserved |
-
+            {{ config('app.name') }}. All Rights Reserved
+            <br>
             Designed & Developed by
-            <a href="http://acetrot.com" target="_blank" class="text-white">Acetrot<img src="{{ url('backend/images/acetrot.png') }}" width="24" alt="">
+            <a href="http://acetrot.com" target="_blank" class="text-white">
+                Acetrot<img src="{{ url('backend/images/acetrot.png') }}" width="24" alt="">
             </a>
         </p>
     </div>
-    <div class="footer-section f-section-2">
-    </div>
+    <div class="footer-section f-section-2"></div>
 </div>
+<style>
+    @media (max-width: 547px) {
+        .footer-text {
+            display: block;
+            text-align: left;
+            width: 100%;
+        }
+
+        .footer-text a {
+            display: inline-flex;
+            align-items: center;
+        }
+    }
+
+    @media (min-width: 548px) {
+        .footer-text br {
+            display: none;
+        }
+    }
+</style>
