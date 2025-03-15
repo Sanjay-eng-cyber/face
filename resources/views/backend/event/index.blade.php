630cb8b9396b7390bf335c7d768ad943606ab205
Raeesh-Rayeen-Acetrot <115560229+Raeesh-Rayeen-Acetrot@users.noreply.github.com>
2025-03-15 13:59:25 +0530

    padding fix in mobile view

--- resources/views/backend/event/index.blade.php
+++ resources/views/backend/event/index.blade.php
@@ -17,7 +17,7 @@
 
                         <div class="col-lg-8 col-md-12 col-sm-12  d-flex align-items-center justify-content-lg-end mp-0">
                             <nav aria-label="breadcrumb">
-                                <ol class="breadcrumb breadcrumb-divider pbz ptpb">
+                                <ol class="breadcrumb breadcrumb-divider pbz ptpb" style="line-height: 1;">
                                     <li class="breadcrumb-item"><a href="/">Home</a></li>
                                     <li class="breadcrumb-item active" aria-current="page">
                                         <a href="javascript:void(0);">Events</a>
