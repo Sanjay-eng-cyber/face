6230983ab60ce2fe3aa989d21bbc4e71377186d2
Raeesh-Rayeen-Acetrot <115560229+Raeesh-Rayeen-Acetrot@users.noreply.github.com>
2025-04-05 12:25:26 +0530

    frontend user collapse fix

--- resources/views/backend/layouts/nav.blade.php
+++ resources/views/backend/layouts/nav.blade.php
@@ -352,6 +352,22 @@
                             </a>
                         </li>
 
+                        <li class="menu" style="display: none">
+                            <a href="{{ route('backend.frotend-user.index', $event->id) }}"
+                                aria-expanded="{{ route('backend.frotend-user.index', $event->id) == request()->url() ? 'true' : 'false' }}"
+                                class="dropdown-toggle link-sdb">
+                                <div class="">
+                                  
+                                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M13 9V3H21V9H13ZM3 13V3H11V13H3ZM13 21V11H21V21H13ZM3 21V15H11V21H3ZM5 11H9V5H5V11ZM15 19H19V13H15V19ZM15 7H19V5H15V7ZM5 19H9V17H5V19Z" />
+                                    </svg>
+                                        
+                                    <span class="nav-sidetext">Frontend User</span>
+                                </div>
+                            </a>
+                        </li>
+
+
                         {{-- <li class="menu">
                             <a href="{{ route('backend.user.index') }}"
                                 aria-expanded="{{ route('backend.user.index') == request()->url() ? 'true' : 'false' }}"
